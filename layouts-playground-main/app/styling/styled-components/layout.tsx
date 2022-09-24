db8c977d524f5d45d85538eabce7302b04bc6759
Jason Wiker <Jason.wiker@vercel.com>
2022-09-24 17:31:46 -0600

    Revert "add test"
    This reverts commit 3c85f6b1adec2bb7d5d63e55de6298794beb8a06.

--- layouts-playground-main/app/styling/styled-components/layout.tsx
+++
@@ -1,9 +0,0 @@
-import StyledComponentsRegistry from './registry'
-
-export default function Layout({ children }: { children: JSX.Element }) {
-  return (
-    <StyledComponentsRegistry>
-      {children}
-    </StyledComponentsRegistry>
-  )
-}
--- layouts-playground-main/app/styling/styled-jsx/layout.tsx
+++
@@ -1,9 +0,0 @@
-import StyledJsxRegistry from './registry'
-
-export default function Layout({ children }: { children: JSX.Element }) {
-  return (
-    <StyledJsxRegistry>
-      {children}
-    </StyledJsxRegistry>
-  )
-}
